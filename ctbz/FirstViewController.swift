61a86fc5c34c736a51aab5710918706b3244bce7
Danizavtz <daninove@gmail.com>
2019-02-16 20:47:52 -0200

    Initial Commit

---
+++ ctbz/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ctbz
+//
+//  Created by Daniel Lucena Pires on 16/02/19.
+//  Copyright © 2019 Pessoal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ctbz/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ctbz
+//
+//  Created by Daniel Lucena Pires on 16/02/19.
+//  Copyright © 2019 Pessoal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
